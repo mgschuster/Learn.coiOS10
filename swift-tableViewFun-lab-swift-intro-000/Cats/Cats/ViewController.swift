c6d267c4ee564453b1e76ccc9685abe0221e95a6
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-03 08:59:53 -0500

    Add files via upload

---
+++ swift-tableViewFun-lab-swift-intro-000/Cats/Cats/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Cats
+//
+//  Created by Michael Dippery on 7/16/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    let catBreeds: [String] = [
+        "Abyssinian",
+        "American Shorthair",
+        "Bengal",
+        "Birman",
+        "British Shorthair",
+        "Burmese",
+        "Exotic Shorthair",
+        "Main Coon",
+        "Norwegian Forest",
+        "Persian",
+        "Ragdoll"
+    ]
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return catBreeds.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "KittyCell", for: indexPath)
+        let catName = catBreeds[indexPath.row]
+        cell.textLabel?.text = catName
+        return cell
+    }
+   
+    
+    
+}
